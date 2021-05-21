9613e7f5e27d1896bfc217cb9692d7d68d5db096
Claudia Silvestre <cla.silvestremu@alum.uca.es>
2021-05-21 11:06:40 +0200

    menu

--- Hito 4/EspacioMetronomo/app/src/main/java/es/uca/espaciometronomo/MainActivity.java
+++ Hito 4/EspacioMetronomo/app/src/main/java/es/uca/espaciometronomo/MainActivity.java
@@ -47,7 +47,19 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_reservas) {
+            return true;
+        }
+
+        if (id == R.id.action_programa) {
+            return true;
+        }
+
+        if (id == R.id.action_fechas) {
+            return true;
+        }
+
+        if (id == R.id.action_localizacion) {
             return true;
         }
 
